4cb5fb052ccaa709dfb5a1c9eed5e434b0658aff
umjammer <umjammer@gmail.com>
2020-06-05 18:07:13 +0900

    remove redundancies

--- src/test/java/Main4.java
+++ src/test/java/Main4.java
@@ -5,36 +5,45 @@
  */
 
 import java.nio.file.FileSystem;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.PosixFilePermissions;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
 
+import vavi.net.fuse.Base;
 import vavi.net.fuse.Fuse;
 import vavi.util.Debug;
-
-import vavix.util.Checksum;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 /**
  * Test4. (jimfs, fuse)
+ * <p>
+ * upload
+ * <ul>
+ * <li> create
+ * <li> write
+ * <li> chmod
+ * <li> chmod
+ * <li> chmod
+ * <li> flush
+ * <li> lock
+ * <li> release
+ * </ul>
+ * download
+ * <ul>
+ * <li> open
+ * <li> read
+ * <li> flush
+ * <li> lock
+ * <li> release
+ * </ul>
  *
  * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
  * @version 0.00 2017/03/19 umjammer initial version <br>
@@ -42,143 +51,50 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @DisabledIfEnvironmentVariable(named = "GITHUB_WORKFLOW", matches = ".*")
 public class Main4 {
 
-    static Fuse fuse;
-    static String mountPoint;
-    static FileSystem fs;
+    FileSystem fs;
+    String mountPoint;
+    Map<String, Object> options;
 
-    @BeforeAll
-    public static void before() throws Exception {
+    @BeforeEach
+    public void before() throws Exception {
         System.setProperty("vavi.util.logging.VaviFormatter.extraClassMethod", "co\\.paralleluniverse\\.fuse\\.LoggedFuseFilesystem#log");
-
-//        System.setProperty("vavi.net.fuse.FuseProvider.class", "vavi.net.fuse.javafs.JavaFSFuseProvider");
-        System.setProperty("vavi.net.fuse.FuseProvider.class", "vavi.net.fuse.jnrfuse.JnrFuseFuseProvider");
 
         mountPoint = System.getenv("TEST4_MOUNT_POINT");
 Debug.println("mountPoint: " + mountPoint);
+
         fs = Jimfs.newFileSystem(Configuration.unix());
 
-        Map<String, Object> options = new HashMap<>();
+        options = new HashMap<>();
         options.put("fsname", "jimfs_fs" + "@" + System.currentTimeMillis());
         options.put("noappledouble", null);
         //options.put("noapplexattr", null);
         options.put(vavi.net.fuse.javafs.JavaFSFuse.ENV_DEBUG, false);
         options.put(vavi.net.fuse.javafs.JavaFSFuse.ENV_READ_ONLY, false);
-
-        fuse = Fuse.getFuse();
-        fuse.mount(fs, mountPoint, options);
     }
 
-    /** */
-    private int exec(String... commandLine) throws Exception {
-        ProcessBuilder processBuilder = new ProcessBuilder(commandLine);
-        processBuilder.inheritIO();
-        System.out.println("$ " + String.join(" ", commandLine));
-        Process process = processBuilder.start();
-        process.waitFor();
-        return process.exitValue();
-    }
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "vavi.net.fuse.javafs.JavaFSFuseProvider",
+        "vavi.net.fuse.jnrfuse.JnrFuseFuseProvider",
+        "vavi.net.fuse.fusejna.FuseJnaFuseProvider",
+    })
+    public void test01(String providerClassName) throws Exception {
+        System.setProperty("vavi.net.fuse.FuseProvider.class", providerClassName);
 
-    /**
-     * create
-     * write
-     * chmod
-     * chmod
-     * chmod
-     * flush
-     * lock
-     * release
-     */
-    @Test
-    @Disabled
-    public void testCopyFromLocalToTarget() throws Exception {
-        Path from = Paths.get("src/test/resources/Hello.java");
-        Path toDir = Paths.get(mountPoint, "VAVI_FUSE_TEST4");
-        if (!Files.exists(toDir)) {
-Debug.println("[mkdir] " + toDir);
-            assertEquals(0, exec("/bin/mkdir", toDir.toString()));
-        }
-        Path to = toDir.resolve(from.getFileName());
-        if (Files.exists(to)) {
-Debug.println("[rm] " + to);
-            assertEquals(0, exec("/bin/rm", to.toString()));
-        }
-Debug.println("[cp] " + from + " " + to);
-        assertEquals(0, exec("/bin/cp", from.toString(), to.toString()));
-        assertEquals(0, exec("/bin/ls", "-l", to.toString()));
-        assertEquals(0, exec("/bin/ls", "-l", from.toString()));
-        assertTrue(Files.exists(to));
-        assertEquals(Files.size(from), Files.size(to));
-        assertEquals(Checksum.getChecksum(from), Checksum.getChecksum(to));
-        Debug.println("[rm] " + to);
-        assertEquals(0, exec("/bin/rm", to.toString()));
-Debug.println("[rmdir] " + toDir);
-        assertEquals(0, exec("/bin/rmdir", toDir.toString()));
-        assertFalse(Files.exists(to));
-        assertFalse(Files.exists(toDir));
-    }
+        Base.testFuse(fs, mountPoint, options);
 
-    /**
-     * open
-     * read
-     * flush
-     * lock
-     * release
-     */
-    @Test
-    public void testCopyFromRemoteToTarget() throws Exception {
-        Path from = Paths.get("src/test/resources/Hello.java");
-        Path remoteDir = fs.getPath("/VAVI_FUSE_TEST4");
-        if (!Files.exists(remoteDir)) {
-Debug.println("[_mkdir] " + remoteDir);
-            Files.createDirectory(remoteDir);
-        }
-        Path remote = remoteDir.resolve(from.getFileName().toString());
-Debug.println("[_cp] " + from + " " + remote);
-        Files.copy(from, remote, StandardCopyOption.REPLACE_EXISTING);
-        assertTrue(Files.exists(remote));
-
-        Path toDir = Paths.get("tmp");
-        if (!Files.exists(toDir)) {
-Debug.println("[_mkdir] " + toDir);
-            Files.createDirectory(toDir);
-        }
-        Path fuseDir = Paths.get(mountPoint, remoteDir.getFileName().toString());
-        Path fuse = fuseDir.resolve(remote.getFileName().toString());
-        Path to = toDir.resolve(fuse.getFileName().toString());
-        if (Files.exists(to)) {
-Debug.println("[_rm] " + to);
-            Files.delete(to);
-        }
-Debug.println("[cp] " + fuse + " " + to);
-        assertEquals(0, exec("/bin/cp", fuse.toString(), to.toString()));
-        assertEquals(0, exec("/bin/ls", "-l", fuseDir.toString()));
-Debug.println("[_chmod] 644 " + to);
-        Files.setAttribute(to, "posix:permissions", PosixFilePermissions.fromString("rw-r--r--"));
-        assertEquals(0, exec("/bin/ls", "-l", to.toString()));
-        assertTrue(Files.exists(to));
-        assertEquals(Files.size(fuse), Files.size(to));
-        assertEquals(Checksum.getChecksum(fuse), Checksum.getChecksum(to));
-Debug.println("[rm] " + remote);
-        assertEquals(0, exec("/bin/rm", fuse.toString()));
-Debug.println("[rmdir] " + toDir);
-        assertEquals(0, exec("/bin/rmdir", fuseDir.toString()));
-        assertFalse(Files.exists(fuse));
-        assertFalse(Files.exists(fuseDir));
-    }
-
-    @AfterAll
-    public static void after() throws Exception {
-        fuse.close();
+        fs.close();
     }
 
     /**
      * @param args
      */
     public static void main(String[] args) throws Exception {
-//      System.setProperty("vavi.net.fuse.FuseProvider.class", "vavi.net.fuse.javafs.JavaFSFuseProvider");
-//      System.setProperty("vavi.net.fuse.FuseProvider.class", "vavi.net.fuse.jnrfuse.JnrFuseFuseProvider");
+        Main4 app = new Main4();
+        app.before();
 
-        Main4.before();
+        Fuse.getFuse().mount(app.fs, app.mountPoint, app.options);
+
         CountDownLatch cdl = new CountDownLatch(1);
         cdl.await();
     }
---
+++ src/test/java/vavi/net/fuse/Base.java
@@ -0,0 +1,109 @@
+package vavi.net.fuse;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Map;
+
+import vavi.util.Debug;
+
+import vavix.util.Checksum;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/*
+ * Copyright (c) 2020 by Naohide Sano, All rights reserved.
+ *
+ * Programmed by Naohide Sano
+ */
+
+
+/**
+ * Base.
+ *
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
+ * @version 0.00 2020/06/05 umjammer initial version <br>
+ */
+public class Base {
+
+    /** */
+    private Base() {
+    }
+
+    /** */
+    private static int exec(String... commandLine) throws Exception {
+        ProcessBuilder processBuilder = new ProcessBuilder(commandLine);
+        processBuilder.inheritIO();
+        System.out.println("$ " + String.join(" ", commandLine));
+        Process process = processBuilder.start();
+        process.waitFor();
+        return process.exitValue();
+    }
+
+    /** */
+    public static void testFuse(FileSystem fs, String mountPoint, Map<String, Object> options) throws Exception {
+        try (Fuse fuse = Fuse.getFuse()) {
+            fuse.mount(fs, mountPoint, options);
+
+            // local -> remote
+            Path local = Paths.get("src/test/resources/Hello.java");
+            Path remoteDir = Paths.get(mountPoint, "VAVI_FUSE_TEST4");
+            if (!Files.exists(remoteDir)) {
+Debug.println("[mkdir] " + remoteDir);
+                assertEquals(0, exec("/bin/mkdir", remoteDir.toString()));
+            }
+            Path remote = remoteDir.resolve(local.getFileName());
+            if (Files.exists(remote)) {
+Debug.println("[rm] " + remote);
+                assertEquals(0, exec("/bin/rm", remote.toString()));
+            }
+
+Debug.println("[cp] " + local + " " + remote);
+            assertEquals(0, exec("/bin/cp", local.toString(), remote.toString()));
+            assertEquals(0, exec("/bin/ls", "-l", remote.toString()));
+            assertEquals(0, exec("/bin/ls", "-l", local.toString()));
+            assertTrue(Files.exists(remote));
+            assertEquals(Files.size(local), Files.size(remote));
+            assertEquals(Checksum.getChecksum(local), Checksum.getChecksum(remote));
+
+            // remote -> local
+            Path localTmpDir = Paths.get("tmp");
+            if (!Files.exists(localTmpDir)) {
+Debug.println("[_mkdir] " + localTmpDir);
+                Files.createDirectory(localTmpDir);
+            }
+            Path localTmp = localTmpDir.resolve(remote.getFileName().toString());
+            if (Files.exists(localTmp)) {
+Debug.println("[_rm] " + localTmp);
+                Files.delete(localTmp);
+            }
+
+Debug.println("[cp] " + remote + " " + localTmp);
+            assertEquals(0, exec("/bin/cp", remote.toString(), localTmp.toString()));
+            assertEquals(0, exec("/bin/ls", "-l", remoteDir.toString()));
+Debug.println("[_chmod] 644 " + localTmp);
+            // jimfs doesn't have permissions after creation
+            Files.setAttribute(localTmp, "posix:permissions", PosixFilePermissions.fromString("rw-r--r--"));
+            assertEquals(0, exec("/bin/ls", "-l", localTmp.toString()));
+            assertTrue(Files.exists(localTmp));
+            assertEquals(Files.size(remote), Files.size(localTmp));
+            assertEquals(Checksum.getChecksum(remote), Checksum.getChecksum(localTmp));
+
+            // clean up
+Debug.println("[rm] " + remote);
+            assertEquals(0, exec("/bin/rm", remote.toString()));
+Debug.println("[rmdir] " + localTmpDir);
+            assertEquals(0, exec("/bin/rmdir", remoteDir.toString()));
+            assertFalse(Files.exists(remote));
+            assertFalse(Files.exists(remoteDir));
+Debug.println("[_rm] " + localTmp);
+            Files.delete(localTmp);
+            assertFalse(Files.exists(localTmp));
+        }
+    }
+}
+
+/* */
