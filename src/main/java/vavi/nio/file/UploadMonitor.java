dfebd3eaf493a89449da7b4cbc59c0b5256598d7
umjammer <umjammer@gmail.com>
2020-06-10 17:02:38 +0900

    update UploadMonitor

--- src/main/java/vavi/nio/file/UploadMonitor.java
+++ src/main/java/vavi/nio/file/UploadMonitor.java
@@ -6,8 +6,6 @@
 
 package vavi.nio.file;
 
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,26 +13,29 @@ import java.util.Set;
 /**
  * UploadMonitor.
  *
+ * TODO embed into java7-fs-base
+ * TODO {@link java.util.concurrent.locks.ReentrantReadWriteLock}???
+ *
  * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
  * @version 0.00 2017/03/19 umjammer initial version <br>
  */
-public class UploadMonitor {
+public class UploadMonitor<T> {
 
     /** */
-    private Set<Path> uploadFlags = new HashSet<>();
+    private Set<T> uploadFlags = new HashSet<>();
 
     /** */
-    public void start(Path path) throws IOException {
+    public void start(T path) {
         uploadFlags.add(path);
     }
 
     /** */
-    public void finish(Path path) throws IOException {
+    public void finish(T path) {
         uploadFlags.remove(path);
     }
 
     /** */
-    public boolean isUploading(Path path) throws IOException {
+    public boolean isUploading(T path) {
         return uploadFlags.contains(path);
     }
 }
