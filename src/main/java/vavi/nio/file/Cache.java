e8016df1fc8471172780c505d2d70e6781707894
umjammer <umjammer@gmail.com>
2020-06-18 04:34:57 +0900

    make the key simple

--- src/main/java/vavi/nio/file/Cache.java
+++ src/main/java/vavi/nio/file/Cache.java
@@ -14,8 +14,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import static vavi.nio.file.Util.toPathString;
-
 
 /**
  * Cache.
@@ -24,68 +22,76 @@ import static vavi.nio.file.Util.toPathString;
  * @version 0.00 2017/03/16 umjammer initial version <br>
  */
 public abstract class Cache<T> {
-    /** <NFC normalized path {@link String}, {@link T}> */
-    protected Map<String, T> entryCache = new ConcurrentHashMap<>(); // TODO refresh
+    /** <NFC normalized path {@link Path}, {@link T}> */
+    protected Map<Path, T> entryCache = new ConcurrentHashMap<>(); // TODO refresh
 
-    /** <NFC normalized path {@link String}, {@link List<Path>}> */
-    protected Map<String, List<Path>> folderCache = new ConcurrentHashMap<>(); // TODO refresh
+    /** <NFC normalized path {@link Path}, {@link List<Path>}> */
+    protected Map<Path, List<Path>> folderCache = new ConcurrentHashMap<>(); // TODO refresh
 
     /** There is metadata or not. */
-    public boolean containsFile(Path path) throws IOException {
-        return entryCache.containsKey(toPathString(path));
+    public boolean containsFile(Path path) {
+        return entryCache.containsKey(path);
     }
 
     /** */
-    public T getFile(Path path) throws IOException {
-        return entryCache.get(toPathString(path));
+    public T getFile(Path path) {
+        return entryCache.get(path);
     }
 
     /** */
-    public T putFile(Path path, T entry) throws IOException {
-        return entryCache.put(toPathString(path), entry);
+    public T putFile(Path path, T entry) {
+        return entryCache.put(path, entry);
     }
 
     /** There are children's metadata or not. */
-    public boolean containsFolder(Path path) throws IOException {
-        return folderCache.containsKey(toPathString(path));
+    public boolean containsFolder(Path path) {
+        return folderCache.containsKey(path);
     }
 
     /** Gets children path. */
-    public List<Path> getFolder(Path path) throws IOException {
-        return folderCache.get(toPathString(path));
+    public List<Path> getFolder(Path path) {
+        return folderCache.get(path);
     }
 
     /**
      * @return -1 if path does not exist
      */
-    public int getChildCount(Path path) throws IOException {
+    public int getChildCount(Path path) {
         return containsFolder(path) ? getFolder(path).size() : -1;
     }
 
     /** */
-    public List<Path> putFolder(Path path, List<Path> children) throws IOException {
-        return folderCache.put(toPathString(path), children);
+    public List<Path> putFolder(Path path, List<Path> children) {
+        return folderCache.put(path, children);
     }
 
     /** */
-    public void addEntry(Path path, T entry) throws IOException {
-        entryCache.put(toPathString(path), entry);
+    public void addEntry(Path path, T entry) {
+        entryCache.put(path, entry);
         Path parentPath = path.getParent();
-        List<Path> paths = folderCache.get(toPathString(parentPath));
+        List<Path> paths = folderCache.get(parentPath);
         if (paths == null) {
             paths = new ArrayList<>();
-            folderCache.put(toPathString(parentPath), paths);
+            folderCache.put(parentPath, paths);
         }
         paths.add(path);
     }
 
     /** */
-    public void removeEntry(Path path) throws IOException {
-        entryCache.remove(toPathString(path));
-        List<Path> paths = folderCache.get(toPathString(path.getParent()));
+    public void removeEntry(Path path) {
+        entryCache.remove(path);
+        List<Path> paths = folderCache.get(path.getParent());
         if (paths != null) {
             paths.remove(path);
         }
+    }
+
+    /** */
+    public void moveEntry(Path source, Path target, T entry) {
+        List<Path> children = getFolder(source);
+        removeEntry(source);
+        addEntry(target, entry);
+        putFolder(target, children);
     }
 
     /**
